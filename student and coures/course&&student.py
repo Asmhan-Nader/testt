e50e8bbc87812a57e4d8a46a012af971172743bc
Asmhan-Nader <113780275+Asmhan-Nader@users.noreply.github.com>
2022-09-24 21:07:26 +0300

    Update course&&student.py

--- student and coures/course&&student.py
+++ student and coures/course&&student.py
@@ -1,24 +1,49 @@
 from course import Course
 from student import Student
+def search_fun(search):
+    for i in student_course:
+        if search == i.course_name:
+            print("its exist")
+
+def search_fun2(search):
+    for i in student_list:
+        if search == i.student_num :
+            print("its exist")
 num = int (input("enter num of stu you wana enter"))
 student_list=[]
 student_course=[]
+alpha=["A","B","C"]
+
 for i in range(num):
-    stu = Student(input("enter stu name"), input("enter stu class"))
-    stu1_course = Course(input("enter course name"), input("enter course class"))
+    stu_name= input("enter stu name")
+    stu_class=input("enter stu class")
+    while True:
+        if stu_class in alpha:
+            break
+        stu_class = input("enter stu class A,B,C")
+    stu = Student( stu_name, stu_class )
     student_list.append(stu)
-    student_course.append(stu1_course)
-    stu.add_course(stu1_course)
+
+
+    course_name = input("enter course name")
+    course_class = input("enter course class")
+    while True:
+         if course_class in alpha:
+            break
+         course_class = input("enter stu class A,B,C")
+    cours = Course(course_name,course_class)
+    student_course.append(cours)
+    stu.add_course(cours)
     for i in student_list:
         print(i.get_student())
+
+
+
 search =int(input("enter your num"))
-for i in student_list:
-    if  search == i.student_num:
-        print("its exist")
+search_fun2(search)
+
 search2 =input("enter course name")
-for i in student_course:
-    if  search2 == i.course_name:
-        print("its exist")
+search_fun(search2)
 
 
 
