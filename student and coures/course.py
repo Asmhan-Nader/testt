05be5fef6af51bb87da6d52308917a481d8c5edd
Asmhan-Nader <113780275+Asmhan-Nader@users.noreply.github.com>
2022-10-08 22:48:59 +0300

    Update course.py

--- student and coures/course.py
+++ student and coures/course.py
@@ -3,3 +3,7 @@ class Course:
         self.course_name=course_name
         self.course_class=course_class
         self.course_id=course_id
+
+
+    def get_course_detail(self):
+        print(str(self.course_id)+ "\t\t" + self.course_name+ self.course_class)
