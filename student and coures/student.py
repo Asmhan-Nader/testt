df729a22107ffc1663e5a8ffd7e783eb7c1b2afd
Asmhan-Nader <113780275+Asmhan-Nader@users.noreply.github.com>
2022-10-08 22:49:41 +0300

    Update student.py

--- student and coures/student.py
+++ student and coures/student.py
@@ -4,9 +4,9 @@ from course import Course
 
 
 class Student:
-    def __init__(self, name, student_class):
+    def __init__(self, name, student_class,student_id):
         self.student_name = name
-        self.student_num = random.randint(0, 1000)  # get random stu number
+        self.student_num = student_id  # get random stu number
         self.student_class = student_class
 
     stu_courses = []
@@ -14,13 +14,15 @@ class Student:
     def add_course(self, courses):#courses is an obj so we will pass it as a parameter to function
         if self.student_class == courses.course_class:
             self.stu_courses.append(courses)  # that means I sort class in list
-
+            print("course enrolled succesfully")
+        else:
+            print("invalid input")
     def get_student(self):
+        print(self.student_name)
+        print(self.student_class)
+        print(self.student_num)
         if len(self.stu_courses) == 0:
             print("there is not any courses enrolled")
         else:
             for i in range(len(self.stu_courses)):
                 print(self.stu_courses[i].course_class)
-        print(self.student_name)
-        print(self.student_class)
-        print(self.student_num)
